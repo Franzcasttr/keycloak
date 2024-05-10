987757ee7f33af00e48c91547843e9503ed71b64
Franzcasttr <franciscastro.216.fc@gmail.com>
2024-05-10 15:47:33 +0800

    update security configuration

--- src/main/java/com/food/keycloak/security/SercurityConfig.java
+++ src/main/java/com/food/keycloak/security/SercurityConfig.java
@@ -1,8 +1,10 @@
 package com.food.keycloak.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,14 +16,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
-//@Slf4j
+@Slf4j
 public class SercurityConfig {
     private final JwtAuthConverter jwtAuthConverter;
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/api/v1/products/getAllProduct")
+                                    .permitAll()
+                                .anyRequest()
+                                    .authenticated()
+                );
 
         http
                 .oauth2ResourceServer((oauth2 )-> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
